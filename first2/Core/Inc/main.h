a034a3fb9aa91cd65fa2ad4ce489e77358690e5a
1 <1@A>
2026-02-02 18:11:56 +0900

    26.02.02 mcp2515 lib porting
    https://github.com/eziya/STM32_SPI_MCP2515

--- first2/Core/Inc/main.h
+++ first2/Core/Inc/main.h
@@ -57,12 +57,23 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define SPI1_NSS_MCP_Pin GPIO_PIN_4
+#define SPI1_NSS_MCP_GPIO_Port GPIOA
 #define GPIO_LED1_Pin GPIO_PIN_5
 #define GPIO_LED1_GPIO_Port GPIOA
+#define SPI1_MISO_MCP_Pin GPIO_PIN_6
+#define SPI1_MISO_MCP_GPIO_Port GPIOA
+#define SPI1_MOSI_MCP_Pin GPIO_PIN_7
+#define SPI1_MOSI_MCP_GPIO_Port GPIOA
+#define EXTI_MCP_Pin GPIO_PIN_0
+#define EXTI_MCP_GPIO_Port GPIOB
+#define EXTI_MCP_EXTI_IRQn EXTI0_IRQn
 #define FND_CLK_Pin GPIO_PIN_10
 #define FND_CLK_GPIO_Port GPIOC
 #define FND_DIO_Pin GPIO_PIN_12
 #define FND_DIO_GPIO_Port GPIOC
+#define SPI1_SCK_MCP_Pin GPIO_PIN_3
+#define SPI1_SCK_MCP_GPIO_Port GPIOB
 #define TEMP_DATA_Pin GPIO_PIN_9
 #define TEMP_DATA_GPIO_Port GPIOB
 
